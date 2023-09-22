6057760a434ce0456c0b39313e9c0fc023932b71
Subha-5 <subhasadhu5@gmail.com>
2023-09-23 03:29:20 +0530

    Edit and Delete posts

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -5,7 +5,12 @@ import Image from "next/image";
 import { usePathname, useRouter } from "next/navigation";
 import { useSession } from "next-auth/react";
 
-const PromptCard = ({ post, handleTagClick }) => {
+const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+    const {data: session} = useSession();
+
+    const pathName = usePathname();
+    const router = useRouter();
+
     const [copied, setCopied] = useState("");
 
     function handleCopy () {
@@ -44,7 +49,14 @@ const PromptCard = ({ post, handleTagClick }) => {
                 </div>
             </div>
             <p className="my-4 font-satoshi text-sm text-gray-700">{post.prompt}</p>
-            <p className="font-inter text-sm blue_gradient cursor-pointer" onClick={() => handleTagClick && handleTagClick(post.tag)}>{post.tag}</p>
+            <p className="font-inter text-sm blue_gradient cursor-pointer" onClick={() => handleTagClick && handleTagClick(post.tag)}>#{post.tag}</p>
+
+            {session?.user.id === post.creator._id && pathName === '/profile' && (
+                <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
+                    <p className="font-inter text-sm green_gradient cursor-pointer" onClick={() => handleEdit(post)}>Edit</p>
+                    <p className="font-inter text-sm orange_gradient cursor-pointer" onClick={() => handleDelete(post)}>Delete</p>
+                </div>
+            )}
         </div>
     )
 }
