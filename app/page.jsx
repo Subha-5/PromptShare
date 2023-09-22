0aad3b3f675f887ac48f0e330690cb5ab1b4e25a
Subha-5 <subhasadhu5@gmail.com>
2023-09-22 14:21:49 +0530

    Create Prompt Page added

--- app/page.jsx
+++ app/page.jsx
@@ -8,7 +8,7 @@ const Home = () => {
         <br className="max-md:hidden" />
         <span className="orange_gradient text-center"> AI-Powered Prompts</span>
       </h1>
-        <p className="desc text-center">Promptopia is an open-source AI powered tool for modern world to discover, create and share creative Prompts</p>
+        <p className="desc text-center">PromptShare is an open-source AI powered tool for modern world to discover, create and share creative Prompts</p>
 
         <Feed/>
       </section>
