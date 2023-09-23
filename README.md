6a71e16b088909efe4fdfe316d94fb5b1cc4be88
Subha-5 <subhasadhu5@gmail.com>
2023-09-23 19:57:42 +0530

    Search functionality and View other's profiles

--- README.md
+++ README.md
@@ -12,3 +12,6 @@ Tech Stack : Next.js, Tailwind CSS
 - User Authentication using next-auth (sign in and sign out)
 - Create a post easily
 - View, Edit and Delete posts from personal profile page.
+- Search feed to find posts
+- click on tag to search for related posts
+- click on username to go and view other's profiles
--- components/Feed.jsx
+++ components/Feed.jsx
@@ -12,28 +12,64 @@ const PromptCardList = ({ data, handleTagClick }) => {
 }
 
 const Feed = () => {
+  const [allPosts, setAllPosts] = useState([]);
+
+  // Search states
   const [searchText, setSearchText] = useState('');
-  const [posts, setPosts] = useState([]);
+  const [searchTimeout, setSearchTimeout] = useState(null)
+  const [searchResults, setSearchResults] = useState([]);
 
-  const handleSearchChange = (e) => {
-
-  }
-
-  useEffect(() => {
     const fetchPosts = async () => {
-      const response = await fetch("api/prompt");
-      const data = await response.json();
-      setPosts(data);
-    }
+    const response = await fetch("/api/prompt");
+    const data = await response.json();
+    setAllPosts(data);
+  }
+  useEffect(() => {
     fetchPosts();
   }, [])
+
+  const filterPrompts = (inputText) => {
+    const regex = new RegExp(inputText, "i"); // 'i' flag for case-insensitive search
+    return allPosts.filter(post =>
+      regex.test(post.creator.username) ||
+      regex.test(post.tag) ||
+      regex.test(post.prompt)
+    )
+  }
+
+  const handleSearchChange = (e) => {
+    clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
+
+    // debounce method
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = filterPrompts(e.target.value);
+        setSearchResults(searchResult)
+      }, 500)
+    );
+  }
+
+  const handleTagClick = (tagName) => {
+    setSearchText(tagName);
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = filterPrompts(tagName);
+        setSearchResults(searchResult)
+        console.log(searchResult)
+      }, 500)
+    );
+  }
 
   return (
     <section className="feed">
       <form className="relative w-full flex-center">
         <input type="text" placeholder="Search for a tag or a username" value={searchText} onChange={handleSearchChange} required className="search_input peer" />
       </form>
-      <PromptCardList data={posts} handleTagClick={() => { }} />
+      {
+        searchText ? (<PromptCardList data={searchResults} handleTagClick={handleTagClick} />) : (<PromptCardList data={allPosts} handleTagClick={handleTagClick} />)
+      }
+
     </section>
   )
 }
